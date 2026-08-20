ceacfe0a7cf86169467ef6e0f702cf0891c25bcc
alist-dev <dev@alist.local>
2026-08-21 01:04:57 +0800

    Revert "fix(quark): refresh __puus cookie periodically to keep download signature valid"
    This reverts commit fdcd8fd2dd647db1b573bac394e41ff09c2ed11a.

--- pkg/cookie/cookie.go
+++ pkg/cookie/cookie.go
@@ -57,15 +57,3 @@ func GetStr(cookiesStr, name string) string {
 	}
 	return cookie.Value
 }
-
-// DelStr 从 cookie 串中删除指定名称的 cookie
-func DelStr(cookiesStr, name string) string {
-	cookies := Parse(cookiesStr)
-	for i, cookie := range cookies {
-		if cookie.Name == name {
-			cookies = append(cookies[:i], cookies[i+1:]...)
-			break
-		}
-	}
-	return ToString(cookies)
-}
